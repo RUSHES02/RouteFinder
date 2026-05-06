9c93bf86e4e0b1265e48920fa9e78f959b8dd4d7
RUSHES02 <abhijaanganguly02@gmail.com>
2026-05-07 00:48:38 +0530

    theme

--- app/src/main/java/com/in/routefinder/ui/theme/Type.kt
+++ app/src/main/java/com/in/routefinder/ui/theme/Type.kt
@@ -2,33 +2,124 @@ package com.`in`.routefinder.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.`in`.routefinder.R
 
 // Set of Material typography styles to start with
+val LatoFontFamily = FontFamily(
+    Font(R.font.lato_thin, FontWeight.Thin),
+    Font(R.font.lato_thin_italic, FontWeight.Thin, FontStyle.Italic),
+
+    Font(R.font.lato_light, FontWeight.Light),
+    Font(R.font.lato_light_italic, FontWeight.Light, FontStyle.Italic),
+
+    Font(R.font.lato_regular, FontWeight.Normal),
+    Font(R.font.lato_italic, FontWeight.Normal, FontStyle.Italic),
+
+    Font(R.font.lato_bold, FontWeight.Bold),
+    Font(R.font.lato_bold_italic, FontWeight.Bold, FontStyle.Italic),
+
+    Font(R.font.lato_black, FontWeight.Black),
+    Font(R.font.lato_black_italic, FontWeight.Black, FontStyle.Italic),
+)
+
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 57.sp,
+        lineHeight = 64.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 45.sp,
+        lineHeight = 52.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 36.sp,
+        lineHeight = 44.sp
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 32.sp,
+        lineHeight = 40.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 28.sp,
+        lineHeight = 36.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 21.sp,
+        lineHeight = 32.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        lineHeight = 18.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = LatoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 14.sp
     )
-    */
 )
